94651ec6568c33b9312a6878f7c8a8d91e5507e1
Nox <patrik.tuominen@outlook.com>
2023-07-27 20:47:04 +0300

    💥 feat: Auth with Discord, login.ts needs work

---
+++ pages/auth/login.ts
@@ -0,0 +1,15 @@
+import { NextApiHandler } from 'next';
+
+const { DISCORD_CLIENT_ID, DISCORD_REDIRECT_URI } = process.env;
+
+const scopes = ['identify', 'email'];
+
+const discordAuthUrl = `https://discord.com/api/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&redirect_uri=${DISCORD_REDIRECT_URI}&response_type=code&scope=${scopes.join(' ')}`;
+
+const handler: NextApiHandler = (_req, res) => {
+  const data = { url: discordAuthUrl };
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(data));
+};
+
+export default handler;
